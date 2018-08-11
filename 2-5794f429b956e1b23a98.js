059ec89be9727ea11e5d32a4e13e399f71278268
Mahipatsinh Jadav <mj@mojaave.com>
2018-08-12 04:02:31 +0530

    Have designed new site

---
+++ 2-5794f429b956e1b23a98.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{184:function(a){a.exports={pages:[{componentChunkName:"component---node-modules-gatsby-plugin-offline-app-shell-js",jsonName:"offline-plugin-app-shell-fallback-a30",path:"/offline-plugin-app-shell-fallback/"}],dataPaths:{"dev-404-page-5f9":"920/path---dev-404-page-5-f-9-fab-NZuapzHg3X9TaN1iIixfv1W23E","offline-plugin-app-shell-fallback-a30":"520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E"}}}}]);
+//# sourceMappingURL=2-5794f429b956e1b23a98.js.map
--- README.md
+++
@@ -1,1 +0,0 @@
-# mhjadav.github.io
